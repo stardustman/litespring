d240285d7680caf28d620289ff804658b21fe008
stardustman <aoyunyoung@gmail.com>
2018-08-07 20:54:03 +0800

    refactor BeanFactoryTest,将一些公共的部分放到 @Before 钩子函数里去

--- src/test/java/org/litespring/test/v1/BeanFactoryTest.java
+++ src/test/java/org/litespring/test/v1/BeanFactoryTest.java
@@ -3,35 +3,52 @@ package org.litespring.test.v1;
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.litespring.beans.BeanDefinition;
 import org.litespring.beans.factory.BeanCreationException;
 import org.litespring.beans.factory.BeanDefinitionStoreException;
-import org.litespring.beans.factory.BeanFactory;
+import org.litespring.beans.factory.xml.XMLBeanDefinitionReader;
 import org.litespring.beans.support.DefaultBeanFactory;
 import org.litespring.service.v1.PetStoreService;
 
-
-
-
 public class BeanFactoryTest {
-
+	 DefaultBeanFactory factory = null;
+	 XMLBeanDefinitionReader reader = null;
+	 
+	 /**
+	  * 每一个测试用例都会执行 before 作为钩子 hook
+	  */
+     @Before
+     public void setUP(){
+    	factory = new DefaultBeanFactory();
+    	System.out.println(factory);
+ 		reader = new XMLBeanDefinitionReader(factory);
+ 		System.out.println(reader);
+     }
+	
+	
 	@Test
 	public void testGetBean() {
-		//1:解析XML获取bean工厂
-		BeanFactory factory = new DefaultBeanFactory("petstore-v1.xml");
-		//2:获取petStore的bean定义
-		BeanDefinition bd = factory.getBeanDefinition("petStore");
-        //3:判断类型
-		assertEquals("org.litespring.service.v1.PetStoreService", bd.getBeanClassName());
-		//4:得到实例
+		/*DefaultBeanFactory factory = new DefaultBeanFactory();
+		XMLBeanDefinitionReader reader = new XMLBeanDefinitionReader(factory);*/
+		
+		reader.loadBeanDefinitions("petstore-v1.xml");
+	
+		BeanDefinition bd = (BeanDefinition) factory.getBeanDefinition("petStore");
+		// 3:判断类型
+		assertEquals("org.litespring.service.v1.PetStoreService",bd.getBeanClassName() );
+		// 4:得到实例
 		PetStoreService petStore = (PetStoreService) factory.getBean("petStore");
 		assertNotNull(petStore);
 	}
-	
+
 	@Test
-	public void testInvalidBean(){
-		BeanFactory factory = new DefaultBeanFactory("petstore-v1.xml");
+	public void testInvalidBean() {
+		/*DefaultBeanFactory factory = new DefaultBeanFactory();
+		XMLBeanDefinitionReader reader = new XMLBeanDefinitionReader(factory);*/
+
+		reader.loadBeanDefinitions("petstore-v1.xml");
 		try {
 			factory.getBean("invalidBean");
 		} catch (BeanCreationException e) {
@@ -41,10 +58,13 @@ public class BeanFactoryTest {
 	}
 
 	@Test
-	public void testInvalidXML(){
+	public void testInvalidXML() {
 
 		try {
-			new DefaultBeanFactory("xxx.xml");
+			DefaultBeanFactory factory = new DefaultBeanFactory();
+			XMLBeanDefinitionReader reader = new XMLBeanDefinitionReader(factory);
+			
+			reader.loadBeanDefinitions("xxxx.xml");
 		} catch (BeanDefinitionStoreException e) {
 			return;
 		}
