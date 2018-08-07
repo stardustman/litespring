c2286d78deac1c8995ec2b3626412303e5a3bf78
stardustman <aoyunyoung@gmail.com>
2018-08-07 12:27:08 +0800

    refactor code directory

---
+++ src/main/java/org/litespring/beans/factory/BeanCreationException.java
@@ -0,0 +1,16 @@
+package org.litespring.beans.factory;
+
+import org.litespring.beans.BeansException;
+
+public class BeanCreationException extends BeansException {
+
+	public BeanCreationException(String msg) {
+		super(msg);
+		
+	}
+
+	public BeanCreationException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
