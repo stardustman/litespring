e0b5a5066f0ff23dad074081abcd0bca48886f3b
stardustman <aoyunyoung@gmail.com>
2018-08-07 22:21:15 +0800

    修改ApplicationContextTest

--- src/test/java/org/litespring/test/v1/ApplicationContextTest.java
+++ src/test/java/org/litespring/test/v1/ApplicationContextTest.java
@@ -1,6 +1,5 @@
 package org.litespring.test.v1;
 
-import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Test;
---
+++ src/test/java/org/litespring/test/v1/ResourceTest.java
@@ -0,0 +1,61 @@
+package org.litespring.test.v1;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.litespring.core.io.ClassPathResource;
+import org.litespring.core.io.FileSystemResource;
+import org.litespring.core.io.Resouce;
+
+public class ResourceTest {
+
+	@Test
+	public void testClassPathResource() {
+		Resouce resource = new ClassPathResource("petstore-v1.xml");
+		InputStream is = null;
+		try {
+			try {
+				is = resource.getInputStream();
+				Assert.assertNotNull(is);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} 
+		 finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	@Test
+	public void testFileSystemResource(){
+		Resouce resource = new FileSystemResource("E:\\petstore-v1.xml");
+		InputStream is = null;
+		try {
+			try {
+				is = resource.getInputStream();
+				Assert.assertNotNull(is);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} 
+		 finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
