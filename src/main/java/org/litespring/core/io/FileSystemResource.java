229cdf51548cb43d2676e072babde680b5f0b041
stardustman <aoyunyoung@gmail.com>
2018-08-10 21:40:49 +0800

    refactor FileSystemResource Constructor

--- src/main/java/org/litespring/core/io/FileSystemResource.java
+++ src/main/java/org/litespring/core/io/FileSystemResource.java
@@ -1,26 +1,34 @@
 package org.litespring.core.io;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.litespring.utils.Assert;
-
-
+import org.litespring.utils.ClassUtils;
 
 public class FileSystemResource implements Resource {
 	private String path;
-	
-	private  File file;
+
+	private File file;
+
+	private ClassLoader classLoader;
 
 	public FileSystemResource(String path) {
-		Assert.notNull(path,"path must not be null");
-		this.path = path;
-		this.file = new File(path);
+	     this(path, (ClassLoader)null);
 	}
+
+	public FileSystemResource(String path, ClassLoader classLoader) {
+		this.path = path;
+		this.classLoader = (classLoader != null) ? classLoader : ClassUtils.getDefaultClassLoader();
+	}
+
 	public InputStream getInputStream() throws IOException {
-		return new FileInputStream(file);
+		InputStream is = this.classLoader.getResourceAsStream(path);
+		if (is == null) {
+			throw new FileNotFoundException(path + " can be opened");
+		}
+		return is;
 	}
 
 	public String getDescription() {
