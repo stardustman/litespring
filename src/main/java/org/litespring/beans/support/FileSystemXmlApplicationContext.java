f65550122f1ecfb07deb8009eddb3d3ad5b1876b
stardustman <aoyunyoung@gmail.com>
2018-08-12 19:06:14 +0800

    refactor FileSystemXmlApplicationContext

--- src/main/java/org/litespring/beans/support/FileSystemXmlApplicationContext.java
+++ src/main/java/org/litespring/beans/support/FileSystemXmlApplicationContext.java
@@ -2,34 +2,37 @@ package org.litespring.beans.support;
 
 import org.litespring.beans.factory.xml.XMLBeanDefinitionReader;
 import org.litespring.context.ApplicationContext;
+import org.litespring.core.io.ClassPathResource;
 import org.litespring.core.io.FileSystemResource;
 import org.litespring.core.io.Resource;
-
+import org.litespring.utils.ClassUtils;
 
 public class FileSystemXmlApplicationContext extends AbstractApplicationContext {
 
 	private DefaultBeanFactory factory = null;
+
 	public FileSystemXmlApplicationContext(String configFile) {
-		//second refactor
-	    /* factory = new DefaultBeanFactory();
-	     XMLBeanDefinitionReader reader = new XMLBeanDefinitionReader(factory);
-	     Resource resource = new FileSystemResource(configFile);
-	     reader.loadBeanDefinitions(resource);*/
-	     
-	     //first refactor
-	     //reader.loadBeanDefinitions(configFile);
-		
+		// second refactor
+		/*
+		 * factory = new DefaultBeanFactory(); XMLBeanDefinitionReader reader =
+		 * new XMLBeanDefinitionReader(factory); Resource resource = new
+		 * FileSystemResource(configFile); reader.loadBeanDefinitions(resource);
+		 */
+
+		// first refactor
+		// reader.loadBeanDefinitions(configFile);
+
 		super(configFile);
 	}
-	@Override
-	protected Resource getResourceByPath(String configFile) {
-		return new FileSystemResource(configFile);
-	}
-	
 
-	//second refactor
-	/*public Object getBean(String beanID) {
-		return factory.getBean(beanID);
-	}*/
+	@Override
+	protected Resource getResourceByPath(String path) {
+		return new FileSystemResource(path);
+	}
+
+	// second refactor
+	/*
+	 * public Object getBean(String beanID) { return factory.getBean(beanID); }
+	 */
 
 }
