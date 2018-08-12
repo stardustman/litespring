b7b975ebb9c8b9c26b55d3469d20e0b315d92011
stardustman <aoyunyoung@gmail.com>
2018-08-12 22:16:25 +0800

    add DefaultSingletonBeanFactoryRegistry to implement SingletonBeanFactoryRegistry

---
+++ src/main/java/org/litespring/beans/support/DefaultSingletonBeanRegistry.java
@@ -0,0 +1,37 @@
+package org.litespring.beans.support;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.swing.text.StyledEditorKit.UnderlineAction;
+
+import org.litespring.beans.factory.config.SingletonBeanFactory;
+import org.litespring.utils.Assert;
+
+/**
+ * 
+ * @author yang'aoyun
+ * @email 2652207782@qq.com
+ * @date 2018年8月12日 下午7:44:36
+ * @version 1.0
+ * @description 默认的单例注册类
+ */
+public class DefaultSingletonBeanRegistry implements SingletonBeanFactory {
+	private final Map<String, Object> singletonObjects = new ConcurrentHashMap<String, Object>(64);
+
+	public void registerSingleton(String beanName, Object singletonObject) {
+		Assert.notNull(beanName, "'beanName' must not be null");
+		Object oldObject = this.singletonObjects.get(beanName);
+		if (oldObject != null) {
+			throw new IllegalStateException("Could not register object [" + singletonObject + "]" + "under bean name'"
+					+ beanName + "': there is alreay object [" + oldObject + "]");
+		}
+		this.singletonObjects.put(beanName, singletonObject);
+
+	}
+
+	public Object getSingleton(String beanName) {
+		return this.singletonObjects.get(beanName);
+	}
+
+}
