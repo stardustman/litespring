b9d669c974e8eb5498d9ba61fae15643863098f3
stardustman <aoyunyoung@gmail.com>
2018-08-12 18:55:58 +0800

    change implements ApplicationContext to AbstractApplicationContext

--- src/main/java/org/litespring/beans/support/ClassPathXmlApplicationContext.java
+++ src/main/java/org/litespring/beans/support/ClassPathXmlApplicationContext.java
@@ -8,26 +8,28 @@ import org.litespring.core.io.Resource;
 public class ClassPathXmlApplicationContext extends AbstractApplicationContext {
 
 	private DefaultBeanFactory factory = null;
+
 	public ClassPathXmlApplicationContext(String configFile) {
-		//second refactor
-		/*factory = new DefaultBeanFactory();
-		XMLBeanDefinitionReader reader = new XMLBeanDefinitionReader(factory);
-		Resource resource = new ClassPathResource(configFile);
-		reader.loadBeanDefinitions(resource);*/
-		
-		//first refactor 
-		//reader.loadBeanDefinitions(configFile);
+		// second refactor
+		/*
+		 * factory = new DefaultBeanFactory(); XMLBeanDefinitionReader reader =
+		 * new XMLBeanDefinitionReader(factory); Resource resource = new
+		 * ClassPathResource(configFile); reader.loadBeanDefinitions(resource);
+		 */
+
+		// first refactor
+		// reader.loadBeanDefinitions(configFile);
 		super(configFile);
-		
+
 	}
-    //second refactor
-	/*public Object getBean(String beanID) {
-		return factory.getBean(beanID);
-	}*/
+	// second refactor
+	/*
+	 * public Object getBean(String beanID) { return factory.getBean(beanID); }
+	 */
 
 	@Override
-	protected Resource getResourceByPath(String configFile) {
-		return new ClassPathResource(configFile);
+	protected Resource getResourceByPath(String path) {
+		return new ClassPathResource(path, this.getBeanClassLoader() );
 	}
 
 }
