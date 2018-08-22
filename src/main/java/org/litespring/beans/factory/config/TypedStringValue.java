345dc13c169348311c89128fa43fb9ef3a6c473f
stardustman <aoyunyoung@gmail.com>
2018-08-22 17:12:49 +0800

    add TypedStringValue to express value

---
+++ src/main/java/org/litespring/beans/factory/config/TypedStringValue.java
@@ -0,0 +1,21 @@
+package org.litespring.beans.factory.config;
+
+/**
+ * 
+ * @author yang'aoyun
+ * @email 2652207782@qq.com
+ * @date 2018年8月22日 下午4:04:24
+ * @version 1.0
+ * @description
+ */
+public class TypedStringValue {
+      private String value;
+      public TypedStringValue(String value) {
+	      this.value = value;
+      }
+      
+      public String getValue() {
+		return value;
+	}
+      
+}
