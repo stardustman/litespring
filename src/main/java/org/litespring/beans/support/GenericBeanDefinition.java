fb5332e5ade8eb49083877ff9cd895f66b12c01e
stardustman <aoyunyoung@gmail.com>
2018-08-22 17:15:51 +0800

    modified GenericBeanDefinition to express bean property

--- src/main/java/org/litespring/beans/support/GenericBeanDefinition.java
+++ src/main/java/org/litespring/beans/support/GenericBeanDefinition.java
@@ -1,6 +1,10 @@
 package org.litespring.beans.support;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.PropertyValue;
 
 
 public class GenericBeanDefinition implements BeanDefinition {
@@ -13,6 +17,7 @@ public class GenericBeanDefinition implements BeanDefinition {
     private boolean  singleton = true;
     private boolean prototype = false;
     private String scope = SCOPE_DEFAULT;
+	private List<PropertyValue> propertyValues = new ArrayList<PropertyValue>();
     
     public GenericBeanDefinition(String id,String beanClassName) {
     	this.id = id;
@@ -39,6 +44,10 @@ public class GenericBeanDefinition implements BeanDefinition {
 		this.scope = scope; 
 		this.singleton = SCOPE_SINGLETON.equals(scope) || SCOPE_DEFAULT.equals(scope);
 	    this.prototype = SCOPE_PROTOTYPE.equals(scope);
+	}
+
+	public List<PropertyValue> getPropertyValues() {
+		return this.propertyValues ;
 	}
 
 }
