ff605494230a78ed492b799f89e929a9ffe92098
stardustman <aoyunyoung@gmail.com>
2018-08-23 20:41:09 +0800

    add BeanDefinitionValueResolver to resovle property value

---
+++ src/main/java/org/litespring/beans/support/BeanDefinitionValueResolver.java
@@ -0,0 +1,38 @@
+package org.litespring.beans.support;
+
+import org.litespring.beans.factory.config.RuntimeBeanReference;
+import org.litespring.beans.factory.config.TypedStringValue;
+
+/**
+ * 
+ * @author yang'aoyun
+ * @email 2652207782@qq.com
+ * @date 2018年8月22日 下午5:58:39
+ * @version 1.0
+ * @description 解析beanID --> bean Instance
+ */
+public class BeanDefinitionValueResolver {
+	private final DefaultBeanFactory beanFactory;
+	
+	public BeanDefinitionValueResolver(DefaultBeanFactory beanFactory) {
+		this.beanFactory = beanFactory;
+	}
+	
+	public Object resolveValueIfNecessary(Object value){
+		if(value instanceof RuntimeBeanReference){
+			RuntimeBeanReference ref = (RuntimeBeanReference) value;
+			String refName = ref.getBeanName();
+			Object bean = this.beanFactory.getBean(refName);
+			return bean;
+		} else if(value instanceof TypedStringValue){
+			return ((TypedStringValue) value).getValue();
+		}
+		
+		else{
+			throw new RuntimeException("the value " + value + " has not implemented");
+		}
+	}
+	
+	
+
+}
