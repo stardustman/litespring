76d8dc25c4fee54f3b89fab07681b8872f09e0e8
stardustman <aoyunyoung@gmail.com>
2018-08-28 22:49:37 +0800

    modified custom editor

--- src/main/java/org/litespring/beans/propertyeditors/CustomBooleanEditor.java
+++ src/main/java/org/litespring/beans/propertyeditors/CustomBooleanEditor.java
@@ -12,60 +12,60 @@ import org.litespring.utils.StringUtils;
  * @version 1.0
  * @description 处理<property  value="on"/> value为boolean值
  */
-public class CustomBooleanEditor
-extends PropertyEditorSupport
-{
-public static final String VALUE_TRUE = "true";
-public static final String VALUE_FALSE = "false";
-public static final String VALUE_ON = "on";
-public static final String VALUE_OFF = "off";
-public static final String VALUE_YES = "yes";
-public static final String VALUE_NO = "no";
-public static final String VALUE_1 = "1";
-public static final String VALUE_0 = "0";
-private final String trueString;
-private final String falseString;
-private final boolean allowEmpty;
+public class CustomBooleanEditor extends PropertyEditorSupport {
 
-public CustomBooleanEditor(boolean allowEmpty)
-{
-  this(null, null, allowEmpty);
-}
+	public static final String VALUE_TRUE = "true";
+	public static final String VALUE_FALSE = "false";
 
-public CustomBooleanEditor(String trueString, String falseString, boolean allowEmpty)
-{
-  this.trueString = trueString;
-  this.falseString = falseString;
-  this.allowEmpty = allowEmpty;
-}
+	public static final String VALUE_ON = "on";
+	public static final String VALUE_OFF = "off";
 
-public void setAsText(String text)
-  throws IllegalArgumentException
-{
-  String input = text != null ? text.trim() : null;
-  if ((this.allowEmpty) && (!StringUtils.hasLength(input))) {
-    setValue(null);
-  } else if ((this.trueString != null) && (input.equalsIgnoreCase(this.trueString))) {
-    setValue(Boolean.TRUE);
-  } else if ((this.falseString != null) && (input.equalsIgnoreCase(this.falseString))) {
-    setValue(Boolean.FALSE);
-  } else if ((this.trueString == null) && ((input.equalsIgnoreCase("true")) || (input.equalsIgnoreCase("on")) || (input.equalsIgnoreCase("yes")) || (input.equals("1")))) {
-    setValue(Boolean.TRUE);
-  } else if ((this.falseString == null) && ((input.equalsIgnoreCase("false")) || (input.equalsIgnoreCase("off")) || (input.equalsIgnoreCase("no")) || (input.equals("0")))) {
-    setValue(Boolean.FALSE);
-  } else {
-    throw new IllegalArgumentException("Invalid boolean value [" + text + "]");
-  }
-}
+	public static final String VALUE_YES = "yes";
+	public static final String VALUE_NO = "no";
 
-public String getAsText()
-{
-  if (Boolean.TRUE.equals(getValue())) {
-    return this.trueString != null ? this.trueString : "true";
-  }
-  if (Boolean.FALSE.equals(getValue())) {
-    return this.falseString != null ? this.falseString : "false";
-  }
-  return "";
-}
+	public static final String VALUE_1 = "1";
+	public static final String VALUE_0 = "0";
+
+
+	private final boolean allowEmpty;
+
+
+	public CustomBooleanEditor(boolean allowEmpty) {
+		this.allowEmpty = allowEmpty;
+	}
+	
+
+	@Override
+	public void setAsText(String text) throws IllegalArgumentException {
+		String input = (text != null ? text.trim() : null);
+		if (this.allowEmpty && !StringUtils.hasLength(input)) {
+			// Treat empty String as null value.
+			setValue(null);
+		}
+		else if ((VALUE_TRUE.equalsIgnoreCase(input) || VALUE_ON.equalsIgnoreCase(input) ||
+						VALUE_YES.equalsIgnoreCase(input) || VALUE_1.equals(input))) {
+			setValue(Boolean.TRUE);
+		}
+		else if ((VALUE_FALSE.equalsIgnoreCase(input) || VALUE_OFF.equalsIgnoreCase(input) ||
+						VALUE_NO.equalsIgnoreCase(input) || VALUE_0.equals(input))) {
+			setValue(Boolean.FALSE);
+		}
+		else {
+			throw new IllegalArgumentException("Invalid boolean value [" + text + "]");
+		}
+	}
+
+	@Override
+	public String getAsText() {
+		if (Boolean.TRUE.equals(getValue())) {
+			return VALUE_TRUE;
+		}
+		else if (Boolean.FALSE.equals(getValue())) {
+			return VALUE_FALSE;
+		}
+		else {
+			return "";
+		}
+	}
+
 }
--- src/main/java/org/litespring/beans/propertyeditors/CustomNumberEditor.java
+++ src/main/java/org/litespring/beans/propertyeditors/CustomNumberEditor.java
@@ -15,61 +15,71 @@ import org.litespring.utils.StringUtils;
  * @description 处理 property的value
  */
 
-public class CustomNumberEditor
-  extends PropertyEditorSupport
-{
-  private final Class<? extends Number> numberClass;
-  private final NumberFormat numberFormat;
-  private final boolean allowEmpty;
-  
-  public CustomNumberEditor(Class<? extends Number> numberClass, boolean allowEmpty)
-    throws IllegalArgumentException
-  {
-    this(numberClass, null, allowEmpty);
-  }
-  
-  public CustomNumberEditor(Class<? extends Number> numberClass, NumberFormat numberFormat, boolean allowEmpty)
-    throws IllegalArgumentException
-  {
-    if ((numberClass == null) || (!Number.class.isAssignableFrom(numberClass))) {
-      throw new IllegalArgumentException("Property class must be a subclass of Number");
-    }
-    this.numberClass = numberClass;
-    this.numberFormat = numberFormat;
-    this.allowEmpty = allowEmpty;
-  }
-  
-  public void setAsText(String text)
-    throws IllegalArgumentException
-  {
-    if ((this.allowEmpty) && (!StringUtils.hasText(text))) {
-      setValue(null);
-    } else if (this.numberFormat != null) {
-      setValue(NumberUtils.parseNumber(text, this.numberClass, this.numberFormat));
-    } else {
-      setValue(NumberUtils.parseNumber(text, this.numberClass));
-    }
-  }
-  
-  public void setValue(Object value)
-  {
-    if ((value instanceof Number)) {
-      super.setValue(NumberUtils.convertNumberToTargetClass((Number)value, this.numberClass));
-    } else {
-      super.setValue(value);
-    }
-  }
-  
-  public String getAsText()
-  {
-    Object value = getValue();
-    if (value == null) {
-      return "";
-    }
-    if (this.numberFormat != null) {
-      return this.numberFormat.format(value);
-    }
-    return value.toString();
-  }
-}
+public class CustomNumberEditor extends PropertyEditorSupport {
 
+	private final Class<? extends Number> numberClass;
+
+	private final NumberFormat numberFormat;
+
+	private final boolean allowEmpty;
+
+	public CustomNumberEditor(Class<? extends Number> numberClass, boolean allowEmpty) throws IllegalArgumentException {
+		this(numberClass, null, allowEmpty);
+	}
+
+	public CustomNumberEditor(Class<? extends Number> numberClass,
+			NumberFormat numberFormat, boolean allowEmpty) throws IllegalArgumentException {
+
+		if (numberClass == null || !Number.class.isAssignableFrom(numberClass)) {
+			throw new IllegalArgumentException("Property class must be a subclass of Number");
+		}
+		this.numberClass = numberClass;
+		this.numberFormat = numberFormat;
+		this.allowEmpty = allowEmpty;
+	}
+
+
+	@Override
+	public void setAsText(String text) throws IllegalArgumentException {
+		if (this.allowEmpty && !StringUtils.hasText(text)) {
+			// Treat empty String as null value.
+			setValue(null);
+		}
+		else if (this.numberFormat != null) {
+			// Use given NumberFormat for parsing text.
+			setValue(NumberUtils.parseNumber(text, this.numberClass, this.numberFormat));
+		}
+		else {
+			// Use default valueOf methods for parsing text.
+			setValue(NumberUtils.parseNumber(text, this.numberClass));
+		}
+	}
+
+	@Override
+	public void setValue(Object value) {
+		if (value instanceof Number) {
+			super.setValue(NumberUtils.convertNumberToTargetClass((Number) value, this.numberClass));
+		}
+		else {
+			super.setValue(value);
+		}
+	}
+
+	
+	@Override
+	public String getAsText() {
+		Object value = getValue();
+		if (value == null) {
+			return "";
+		}
+		if (this.numberFormat != null) {
+			// Use NumberFormat for rendering value.
+			return this.numberFormat.format(value);
+		}
+		else {
+			// Use toString method for rendering value.
+			return value.toString();
+		}
+	}
+
+}
