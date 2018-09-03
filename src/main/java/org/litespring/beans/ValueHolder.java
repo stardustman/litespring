b1281df1560d2a6d9f26cb0958ce5cdf9a9b286c
stardustman <aoyunyoung@gmail.com>
2018-09-03 22:08:59 +0800

    add ValueHolder to express constructor-arg attribute

---
+++ src/main/java/org/litespring/beans/ValueHolder.java
@@ -0,0 +1,16 @@
+package org.litespring.beans;
+
+/**
+ * 
+ * @author yang'aoyun
+ * @email 2652207782@qq.com
+ * @date 2018年9月2日 下午9:28:29
+ * @version 1.0
+ * @description
+ */
+public class ValueHolder {
+
+	public ValueHolder(Object value, String type) {
+	}
+
+}
