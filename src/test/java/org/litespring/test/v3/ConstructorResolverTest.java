7f2fb69b22144187446f098a0e7680ae5934e888
stardustman <aoyunyoung@gmail.com>
2018-09-06 23:29:55 +0800

    add ConstructorResolverTest

---
+++ src/test/java/org/litespring/test/v3/ConstructorResolverTest.java
@@ -0,0 +1,41 @@
+package org.litespring.test.v3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.xml.XMLBeanDefinitionReader;
+import org.litespring.beans.support.ConstructorResolver;
+import org.litespring.beans.support.DefaultBeanFactory;
+import org.litespring.core.io.ClassPathResource;
+import org.litespring.core.io.Resource;
+import org.litespring.service.v3.PetStoreService;
+/**
+ * 
+ * @author yang'aoyun
+ * @email 2652207782@qq.com
+ * @date 2018年9月4日 下午8:03:15
+ * @version 1.0
+ * @description
+ */
+public class ConstructorResolverTest {
+
+	@Test
+	public void testAutowireConstructor() {
+		DefaultBeanFactory factory = new DefaultBeanFactory();
+		XMLBeanDefinitionReader reader = new XMLBeanDefinitionReader(factory);
+		Resource resource = new ClassPathResource("petstore-v3.xml");
+		reader.loadBeanDefinitions(resource);
+		BeanDefinition bd = factory.getBeanDefinition("petStore");
+		
+		ConstructorResolver resolver = new ConstructorResolver(factory);
+		
+		PetStoreService petStore = (PetStoreService) resolver.autowireConstructor(bd);
+		
+		Assert.assertEquals(1, petStore.getVersion());
+		Assert.assertNotNull(petStore.getAccountDao());
+		Assert.assertNotNull(petStore.getItemDao());
+	}
+
+}
