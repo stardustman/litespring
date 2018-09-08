b935231187d7159156685075f771de7faf23bdc5
stardustman <aoyunyoung@gmail.com>
2018-09-08 21:44:06 +0800

    modifiy createBean method add use constructor-injection or not

--- src/main/java/org/litespring/beans/support/DefaultBeanFactory.java
+++ src/main/java/org/litespring/beans/support/DefaultBeanFactory.java
@@ -135,15 +135,23 @@ public class DefaultBeanFactory extends DefaultSingletonBeanRegistry
 	 * @return
 	 */
 	private Object instantiateBean(BeanDefinition bd) {
-		ClassLoader cl = this.getBeanClassLoader();
-		String beanClassName = bd.getBeanClassName();
-		try {
-			Class<?> clz = cl.loadClass(beanClassName);
-			return clz.newInstance();
+		
+		//构造器注入
+		if (bd.hasConstructorArgumentValues()) {
+			ConstructorResolver resolver = new ConstructorResolver(this);
+			return resolver.autowireConstructor(bd);
+		} else{
+			ClassLoader cl = this.getBeanClassLoader();
+			String beanClassName = bd.getBeanClassName();
+			try {
+				Class<?> clz = cl.loadClass(beanClassName);
+				return clz.newInstance();
 
-		} catch (Exception e) {
-			throw new BeanCreationException("create bean for " + beanClassName + "failed", e);
+			} catch (Exception e) {
+				throw new BeanCreationException("create bean for " + beanClassName + "failed", e);
+			}
 		}
+		
 
 	}
 
